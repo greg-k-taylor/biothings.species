5591004258a32984728d0034233897cf29ba0853
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2016-06-03 16:32:52 -0700

    I feel like I shouldn't be doing this, but I guess I'm doing it anyway.

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -1,12 +1,14 @@
 import sys
 import os
+from tornado.testing import AsyncHTTPTestCase
+from tornado.web import Application
+from www.api.handlers import return_applist
 
 # Add this directory to python path (contains nosetest_config)
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 from biothings.tests.tests import BiothingTests
 from biothings.tests.settings import NosetestSettings
-from nose.tools import ok_, eq_
 
 ns = NosetestSettings()
 
@@ -14,4 +16,3 @@ class MySpeciesTest(BiothingTests):
     __test__ = True # explicitly set this to be a test class
     # Add extra nosetests here
     pass
-
