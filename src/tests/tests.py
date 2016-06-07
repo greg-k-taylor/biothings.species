622a69d1ae3afeedb0680856f36c3eabb3137099
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2016-06-07 11:36:30 -0700

    Accomodating the changed suite definition.

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from biothings.tests.tests import BiothingTests
 from biothings.tests.settings import BiothingTestSettings
 
-ns = BiothingTestSettings()
+bts = BiothingTestSettings()
 
 class MySpeciesTest(BiothingTests):
     __test__ = True # explicitly set this to be a test class
