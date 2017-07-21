060273c2ca490e0ff07f0283cfa9ba5b011e347a
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2017-07-21 10:17:58 -0700

    Renaming www->web...

--- src/web/api/handlers.py
+++ src/web/api/handlers.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-from biothings.www.api.es.handlers import BiothingHandler
-from biothings.www.api.es.handlers import MetadataHandler
-from biothings.www.api.es.handlers import QueryHandler
-from biothings.www.api.es.handlers import StatusHandler
+from biothings.web.api.es.handlers import BiothingHandler
+from biothings.web.api.es.handlers import MetadataHandler
+from biothings.web.api.es.handlers import QueryHandler
+from biothings.web.api.es.handlers import StatusHandler
 #import logging
 
 def pre_query_builder_hook(inst, options):
--- src/web/settings/__init__.py
+++ src/web/settings/__init__.py
@@ -1,4 +1,4 @@
-from biothings.www.settings import BiothingESWebSettings
+from biothings.web.settings import BiothingESWebSettings
 
 class MySpeciesWebSettings(BiothingESWebSettings):
     # Add app-specific settings functions here
